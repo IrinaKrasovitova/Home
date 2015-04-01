abab7d15d6a10156d075dafb4f1ceea12045a10c
IrinaKrasovitova <irinastyle1996@mail.ru>
2015-04-01 22:42:04 +0300

    Update stdafx.h

--- stdafx.h
+++ stdafx.h
@@ -10,5 +10,15 @@
 #include <stdio.h>
 #include <tchar.h>
 
+#include "dll.h"
+#include "pwdbased.h"
+#include "md5.h"
+#include "sha.h"
+#include "cryptlib.h"
+#include "modes.h"
+#include "filters.h"
+#include "aes.h"
+#include "gost.h"
+
 
 // TODO: Установите здесь ссылки на дополнительные заголовки, требующиеся для программы
